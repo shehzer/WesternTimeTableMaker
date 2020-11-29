add454e7c013e53cacace71111d28a29e389d949
Saud Naumani <saud.naumani@gmail.com>
2020-11-29 01:42:36 -0500

    fixed bug with middleware

--- angular-schedule-maker/server/routes/verifytoken.js
+++ angular-schedule-maker/server/routes/verifytoken.js
@@ -11,6 +11,7 @@ function auth(req,res,next){
     try {
         const verified = jwt.verify(token, process.env.TOKEN_SECRET);
         req.user = verified;
+        next();
     }catch(err){
         res.status(400).send('Invalid Token');
     }
