bf3828ca3d4216b54223352ac77dac4635447811
Saud Naumani <saud.naumani@gmail.com>
2020-11-29 00:24:41 -0500

    validate userr input

--- angular-schedule-maker/server/routes/auth.js
+++ angular-schedule-maker/server/routes/auth.js
@@ -1,11 +1,26 @@
 const router = require('express').Router()
 const User = require('../model/User');
 
-    // router.post('/register', (req,res) => {
-    //     res.send("Registered")
-    // })
+    //Validation
+
+    const Joi = require('@hapi/joi');
+    
+
+    const schema = Joi.object({
+        name: Joi.string().min(6).required(),
+        email: Joi.string().min(6).required().email(),
+        password: Joi.string().min(6).required()
+    });
+
+
+
+
+
 
     router.post('/register', async (req,res)=> {
+        //Validate Response
+       const {error} = schema.validate(req.body);
+       if(error) return res.status(400).send(error.details[0].message);
         const user = new User({
             name: req.body.name,
             email: req.body.email,
