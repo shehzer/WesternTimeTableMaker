438c7cac2819ad6e38806edb5c73daebcfda670b
Saud Naumani <saud.naumani@gmail.com>
2020-11-29 00:42:18 -0500

    implemented error for duplicate email

--- angular-schedule-maker/server/routes/auth.js
+++ angular-schedule-maker/server/routes/auth.js
@@ -1,36 +1,29 @@
 const router = require('express').Router()
 const User = require('../model/User');
-
-    //Validation
-
-    const Joi = require('@hapi/joi');
-    
-
-    const schema = Joi.object({
-        name: Joi.string().min(6).required(),
-        email: Joi.string().min(6).required().email(),
-        password: Joi.string().min(6).required()
-    });
-
-
-
+const {registerValidation} = require('../validation');
 
 
 
     router.post('/register', async (req,res)=> {
         //Validate Response
-       const {error} = schema.validate(req.body);
-       if(error) return res.status(400).send(error.details[0].message);
+        const {error} = registerValidation(req.body);
+        if(error) return res.status(400).send(error.details[0].message);
+
+        //Check if user is already registered
+        const emailExists = await User.findOne({email: req.body.email});
+        if(emailExists) return res.status(400).send('Email Already exists');
+
+        //Create New User
         const user = new User({
             name: req.body.name,
             email: req.body.email,
             password: req.body.password
         });
-    try{
-        const savedUser = await user.save();
-        res.send(savedUser);
-    } catch(err){
-        res.status(400).send(err);
+        try{
+            const savedUser = await user.save();
+            res.send(savedUser);
+        } catch(err){
+            res.status(400).send(err);
     }
 
     });
